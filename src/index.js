a2ee1e48f198934cf16585cd141a7db3c7261c59
Sveta K <test@test.test>
2020-10-04 16:37:33 +0100

    init: start human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    const exceptionsTen = ["ten", "eleven", "twelve"]
+    const exceptionsOther = {
+        2: "twen",
+        3: "thir",
+        4: "for",
+        5: "fif",
+        8: "eigh"
+    }
+
+
 }
+
